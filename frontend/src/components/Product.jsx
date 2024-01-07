d35a2fbffe3745dc07fb2ff1ddbfb0b33a93ac1b
bakemonostan <ehizomokhomion@gmail.com>
2024-01-07 04:12:17 +0100

    create homeScreen and product list component

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,30 @@
+import { Card } from "react-bootstrap";
+// import { Link } from 'react-router-dom';
+// import Rating from './Rating';
+
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div" className="product-title">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+
+        <Card.Text as="h3">
+          {/* <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          /> */}
+          ${product.price}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+export default Product;
