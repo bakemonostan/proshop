abec9b44696bb2d7174ae25b35c2d79f264c64e5
bakemonostan <ehizomokhomion@gmail.com>
2024-01-07 07:23:09 +0100

    add axios and mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log("hello");
+import products from "./data/products.js";
+
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+// all products
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+// single product
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = products.find((p) => p._id === productId);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`server running on port ${port}`));
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,10 +1,20 @@
 import { Link, useParams } from "react-router-dom";
-import products from "../products";
 import { Card, Col, Row, ListGroup, Button, Image } from "react-bootstrap";
 import Rating from "../components/Rating";
+import axios from "axios";
+import { useEffect, useState } from "react";
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find((product) => product._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
+
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
